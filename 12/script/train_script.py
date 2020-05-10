b13a8f0e4891cfed885b16f520a534fa8a7c7df8
spica88 <kentaro@MacBook-Pro-2.local>
2020-05-10 18:31:41 +0900

    fourth

--- 12/script/train_script.py
+++ 12/script/train_script.py
@@ -2,4 +2,4 @@ import train.train_ccae_model
 
 
 if __name__ == "__main__":
-    train.train_ccae_model.run_with_generator(with_volume=False)
+    train.train_ccae_model.run_with_generator(with_volume=True)
